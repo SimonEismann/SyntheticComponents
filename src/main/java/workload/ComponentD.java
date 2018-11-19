b192cf8323848c4c1670db72434ee83bb7ac3b1b
Simon <simon.eismann@uni-wuerzburg.de>
2018-11-19 16:08:13 +0100

    adapted workloads

--- src/main/java/workload/ComponentD.java
+++ src/main/java/workload/ComponentD.java
@@ -7,7 +7,7 @@ public class ComponentD extends Workload {
 			
 	@Override
 	public String performWork() throws UnsupportedEncodingException, IOException {
-		double result = performNormalWork(0.015, 0.005);
+		double result = performNormalWork(0.001 * 1000, 0.005 * 1000);
 		return "Served at Component D! --> " + result;
 	}
 
