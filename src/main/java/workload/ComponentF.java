40a867f1e31cc6f7973a5d6de6e3c29d159c8f75
Simon <simon.eismann@uni-wuerzburg.de>
2018-11-19 16:57:09 +0100

    minor workload updates

--- src/main/java/workload/ComponentF.java
+++ src/main/java/workload/ComponentF.java
@@ -4,7 +4,7 @@ public class ComponentF extends Workload {
 	
 	@Override
 	public String performWork() {
-		double result = performBranchWork(0.8, 0.005 * 1000, 0.025 * 1000);
+		double result = performBranchWork(0.8, 0.05 * 1000, 0.25 * 1000);
 		return "Served at Component F! --> " + result;
 	}
 
