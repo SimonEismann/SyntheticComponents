6b99c757af1f7df83a600973598c03fbbe76eb72
Simon <simon.eismann@uni-wuerzburg.de>
2018-11-20 10:41:01 +0100

    removed threadpool limit

--- src/main/java/workload/ComponentE.java
+++ src/main/java/workload/ComponentE.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 public class ComponentE extends Workload {
-	private static int limit = 2;
-	private static int count = 0;
-	private static Object lock = new Object();
 
 	public String ipF;
 	public String ipG;
@@ -18,28 +15,11 @@ public class ComponentE extends Workload {
 
 	@Override
 	public String performWork() throws UnsupportedEncodingException, IOException, InterruptedException {
-		while (true) {
-			boolean ok;
-			synchronized (lock) {
-				ok = count < limit;
-			}
-			if (ok) {
-				synchronized (lock) {
-					count++;
-				}
-				double result = performExpWork(0.0005 * 1000);
-				String result2 = callTo(ipF, true);
-				String result3 = callTo(ipG, false);
-				synchronized (lock) {
-					count--;
-				}
-				return "Served at Component E! --> " + result + "\n\t" + result2.replace("\t", "\t\t") + "\n\t"
-						+ result3.replace("\t", "\t\t");
-			} else {
-				Thread.sleep(100);
-				return performWork();
-			}
-		}
+		double result = performExpWork(0.0005 * 1000);
+		String result2 = callTo(ipF, true);
+		String result3 = callTo(ipG, false);
+		return "Served at Component E! --> " + result + "\n\t" + result2.replace("\t", "\t\t") + "\n\t"
+				+ result3.replace("\t", "\t\t");
 	}
 
 }
