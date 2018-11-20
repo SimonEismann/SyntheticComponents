15cf6ff012240ccbb08bc4fc42b83f8d7d04ef40
Simon <simon.eismann@uni-wuerzburg.de>
2018-11-20 10:24:03 +0100

    reduced workload once again

--- src/main/java/workload/ComponentB.java
+++ src/main/java/workload/ComponentB.java
@@ -15,7 +15,7 @@ public class ComponentB extends Workload {
 	
 	@Override
 	public String performWork() throws UnsupportedEncodingException, IOException {
-		double result = performConstantWork(0.015 * 1000);
+		double result = performConstantWork(0.005 * 1000);
 		String result2 = callTo(ipC, true);
 		String result3 = callTo(ipE, false);
 		return "Served at Component B! --> " + result + "\n\t"+ result2.replace("\t", "\t\t") + "\n\t"+ result3.replace("\t", "\t\t");
--- src/main/java/workload/ComponentD.java
+++ src/main/java/workload/ComponentD.java
@@ -7,7 +7,7 @@ public class ComponentD extends Workload {
 			
 	@Override
 	public String performWork() throws UnsupportedEncodingException, IOException {
-		double result = performNormalWork(0.01 * 1000, 0.005 * 1000);
+		double result = performNormalWork(0.003 * 1000, 0.003 * 1000);
 		return "Served at Component D! --> " + result;
 	}
 
