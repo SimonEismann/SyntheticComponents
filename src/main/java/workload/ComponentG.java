667271311e1fe574a718cc1e44791a0931597262
Simon <simon.eismann@uni-wuerzburg.de>
2018-11-21 10:21:09 +0100

    reduced workload a little

--- src/main/java/workload/ComponentG.java
+++ src/main/java/workload/ComponentG.java
@@ -4,7 +4,7 @@ public class ComponentG extends Workload {
 	
 	@Override
 	public String performWork() {
-		double result = performExpWork(0.04 * 1000);
+		double result = performExpWork(0.01 * 1000);
 		return "Served at Component G! --> " + result;
 	}
 
