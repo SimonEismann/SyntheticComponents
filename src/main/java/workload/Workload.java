4cc9a916d971c43423d1d7ebd88ed280d5fe8a14
Simon <simon.eismann@uni-wuerzburg.de>
2018-11-24 14:20:32 +0100

    fixed logging

--- src/main/java/workload/Workload.java
+++ src/main/java/workload/Workload.java
@@ -67,7 +67,7 @@ public abstract class Workload {
 					break;
 			}
 			long toc = System.nanoTime();
-			LoggingInternal.globalQueue.add((toc-tic) + "," + tic + "," + toc + "," + start );
+			LoggingInternal.globalQueue.add((toc-tic) + "," + tic + "," + toc + "," + (toc - start));
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} finally {
