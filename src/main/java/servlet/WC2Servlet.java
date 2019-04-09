72324381a36d998dcee212ab94ffc69bbc71068c
Simon <simon.eismann@uni-wuerzburg.de>
2019-04-09 13:25:41 +0200

    draft 0.1

---
+++ src/main/java/servlet/WC2Servlet.java
@@ -0,0 +1,60 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/wc2")
+public class WC2Servlet extends HttpServlet {
+	private Random rand = new Random();
+	
+	private static final long serialVersionUID = 1L;
+
+    public WC2Servlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			performExpWork(0.08 * 1000);
+			response.getWriter().append("WC2:80ms");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected double performExpWork(double mean) {
+		double lambda = 1 / mean;
+		double delay = Math.log(1 - rand.nextDouble()) / (-lambda);
+		double result = performConstantWork(delay);
+		return result;
+	}
+
+	protected double performConstantWork(double milliseconds) {
+		try {
+			long tic = System.nanoTime();
+			sem.acquire();
+			long start = System.nanoTime();
+			while (true) {
+				if (System.nanoTime() - start > milliseconds * 1000000)
+					break;
+			}
+			long toc = System.nanoTime();
+			LoggingInternal.globalQueue.add((toc - tic) + "," + sem.getQueueLength() + "," + tic + "," + (toc - start));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			sem.release();
+		}
+		return 1;
+	}
+}
---
+++ src/main/java/servlet/WC3Servlet.java
@@ -0,0 +1,65 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/wc3")
+public class WC3Servlet extends HttpServlet {
+	private Random rand = new Random();
+	
+	private static final long serialVersionUID = 1L;
+
+    public WC3Servlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			performNormalWork(0.010 * 1000, 0.002 * 1000);
+			response.getWriter().append("WC3:10ms+-2ms");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected double performNormalWork(double mean, double std) {
+		double result = performConstantWork(rand.nextGaussian() * std + mean);
+		return result;
+	}
+
+	protected double performExpWork(double mean) {
+		double lambda = 1 / mean;
+		double delay = Math.log(1 - rand.nextDouble()) / (-lambda);
+		double result = performConstantWork(delay);
+		return result;
+	}
+
+	protected double performConstantWork(double milliseconds) {
+		try {
+			long tic = System.nanoTime();
+			sem.acquire();
+			long start = System.nanoTime();
+			while (true) {
+				if (System.nanoTime() - start > milliseconds * 1000000)
+					break;
+			}
+			long toc = System.nanoTime();
+			LoggingInternal.globalQueue.add((toc - tic) + "," + sem.getQueueLength() + "," + tic + "," + (toc - start));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			sem.release();
+		}
+		return 1;
+	}
+}
