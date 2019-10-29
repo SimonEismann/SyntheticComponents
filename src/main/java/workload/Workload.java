251f84a195aef5ff4bb339d3aa6368d7d885b084
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2019-10-29 14:33:55 +0100

    Update Workload.java

--- src/main/java/workload/Workload.java
+++ src/main/java/workload/Workload.java
@@ -24,7 +24,7 @@ import servlet.LoggingInternalF;
 import servlet.LoggingInternalG;
 
 public abstract class Workload {
-	public static Semaphore semaphore = new Semaphore(2, false);
+	public static Semaphore semaphore = new Semaphore(2, true);
 	
 	private Random rand = new Random();
 
