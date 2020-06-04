2448949252b4111fc263c5829e4d18d15913daee
Simon Trapp <simon.trapp@stud-mail.uni-wuerzburg.de>
2020-06-04 09:48:22 +0200

    component hiwiC bugfix

--- src/main/java/workload/ComponentHiwiC.java
+++ src/main/java/workload/ComponentHiwiC.java
@@ -4,7 +4,7 @@ public class ComponentHiwiC extends Workload {
 	
 	@Override
 	public String performWork() {
-		double result = performBranchWork(0.2, 5, 50);
+		double result = performBranchWork(0.8, 5, 50);
 		return "Served at Component C! --> " + result;
 	}
 
