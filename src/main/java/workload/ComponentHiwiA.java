6e80761b4518b918ea17064eaa2f0fe34284e4bb
Simon Trapp <simon.trapp@stud-mail.uni-wuerzburg.de>
2021-04-15 14:01:13 +0200

    updated emptyhost components

--- src/main/java/workload/ComponentHiwiA.java
+++ src/main/java/workload/ComponentHiwiA.java
@@ -7,15 +7,15 @@ public class ComponentHiwiA extends Workload {
 
 	public String ipC;
 	public String ipB;
-	
+
 	public ComponentHiwiA(String ipB, String ipC) {
 		this.ipC = ipC;
 		this.ipB = ipB;
 	}
-	
+
 	@Override
 	public String performWork() throws UnsupportedEncodingException, IOException {
-		double result = performConstantWork(10);
+		double result = performExpWork(30);
 		String result2 = callTo(ipB);
 		String result3 = callTo(ipC);
 		return "Served at Component A! --> " + result + "\n\t"+ result2.replace("\t", "\t\t") + "\n\t"+ result3.replace("\t", "\t\t");
--- src/main/java/workload/ComponentHiwiC.java
+++ src/main/java/workload/ComponentHiwiC.java
@@ -1,10 +1,10 @@
 package workload;
 
 public class ComponentHiwiC extends Workload {
-	
+
 	@Override
 	public String performWork() {
-		double result = performBranchWork(0.8, 5, 50);
+		double result = performExpWork(25);
 		return "Served at Component C! --> " + result;
 	}
 
